be2a9613bc7b051d8aab90d65b61ade77a8f638a
turapa62-afk <turapa62@gmail.com>
2026-05-01 12:32:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,32 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// direkt index.html ver
+// index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// 📦 SAVE DOSYASI
+const SAVE_FILE = "map.json";
+
+// 🧠 pixel load (kalıcı sistem)
 let pixels = {};
+
+if (fs.existsSync(SAVE_FILE)) {
+  try {
+    pixels = JSON.parse(fs.readFileSync(SAVE_FILE));
+    console.log("Map yüklendi");
+  } catch (e) {
+    console.log("Map yüklenemedi, sıfır başlatıldı");
+    pixels = {};
+  }
+}
 
 io.on("connection", (socket) => {
   console.log("Bir oyuncu bağlandı:", socket.id);
@@ -21,6 +36,10 @@ io.on("connection", (socket) => {
 
   socket.on("placePixel", (data) => {
     pixels[`${data.x},${data.y}`] = data.color;
+
+    // 💾 her pixelde kaydet
+    fs.writeFileSync(SAVE_FILE, JSON.stringify(pixels));
+
     io.emit("pixelUpdate", data);
   });
 
