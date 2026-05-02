4461a3d12e215b67d087b384cf8fd81cd02fab00
turapa62-afk <turapa62@gmail.com>
2026-05-02 16:32:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,55 +7,30 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// direkt index.html ver
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// ===== WORLD CONFIG =====
-const WORLD = 800;
-
-// pixel storage
 let pixels = {};
 
-// 🧠 (opsiyonel) pastel başlangıç world
-function generateSoftWorld() {
-  for (let x = 0; x < WORLD; x++) {
-    for (let y = 0; y < WORLD; y++) {
-      // çok hafif pastel random dünya
-      const r = 200 + Math.floor(Math.random() * 55);
-      const g = 200 + Math.floor(Math.random() * 55);
-      const b = 200 + Math.floor(Math.random() * 55);
-
-      pixels[`${x},${y}`] = `rgb(${r},${g},${b})`;
-    }
-  }
-}
-
-// ilk dünya
-generateSoftWorld();
-
 io.on("connection", (socket) => {
-  console.log("🟢 Oyuncu bağlandı:", socket.id);
+  console.log("Bir oyuncu bağlandı:", socket.id);
 
-  // init world
   socket.emit("init", pixels);
 
   socket.on("placePixel", (data) => {
-    const key = `${data.x},${data.y}`;
-
-    pixels[key] = data.color;
-
-    // herkese yay
+    pixels[`${data.x},${data.y}`] = data.color;
     io.emit("pixelUpdate", data);
   });
 
   socket.on("disconnect", () => {
-    console.log("🔴 Oyuncu çıktı:", socket.id);
+    console.log("Oyuncu çıktı:", socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log("🚀 SERVER RUNNING:", PORT);
+  console.log("SERVER RUNNING on port " + PORT);
 });
