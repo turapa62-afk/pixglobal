d582b1a4645b4e817a93f3ceec4ccd370805e291
turapa62-afk <turapa62@gmail.com>
2026-05-02 16:42:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,52 +7,30 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// index.html
+// direkt index.html ver
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// WORLD
 let pixels = {};
 
-// 👇 oyuncu pixel sayacı
-const userPixels = {};
-const MAX_PIXELS = 50;
-
 io.on("connection", (socket) => {
-  console.log("Bağlandı:", socket.id);
+  console.log("Bir oyuncu bağlandı:", socket.id);
 
-  userPixels[socket.id] = 0;
-
-  // world gönder
   socket.emit("init", pixels);
 
   socket.on("placePixel", (data) => {
-    if (!data) return;
-
-    let count = userPixels[socket.id] || 0;
-
-    // 🔥 50 LIMIT (PixUniverse tarzı)
-    if (count >= MAX_PIXELS) {
-      userPixels[socket.id] = MAX_PIXELS - 1;
-      return;
-    }
-
-    userPixels[socket.id]++;
-
     pixels[`${data.x},${data.y}`] = data.color;
-
     io.emit("pixelUpdate", data);
   });
 
   socket.on("disconnect", () => {
-    delete userPixels[socket.id];
-    console.log("Çıktı:", socket.id);
+    console.log("Oyuncu çıktı:", socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log("SERVER RUNNING:", PORT);
+  console.log("SERVER RUNNING on port " + PORT);
 });
